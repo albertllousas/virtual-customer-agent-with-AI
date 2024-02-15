6620d85d4478d15a278ed64afbda938b232fb090
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2024-02-15 14:48:34 +0100

    Change file name

--- app/src/main/kotlin/pets/ChatWithAVirtualAgentService.kt
+++ app/src/main/kotlin/pets/ChatWithAVirtualAgentService.kt
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Service
 import java.util.UUID
 
 const val defaultContextMessage = """
-            You're playing a role of a virtual customer agent in a company Next Gen Pets.
-            Next Gen Pets is a company that sells advanced robotic companions designed with cutting-edge AI, offering 
-            interactive experiences and personalized companionship.
-                    
+            You're playing a role of a virtual customer agent in a company called Next Gen Pets.
+            At Next Gen Pets we sell advanced robotic companions designed with cutting-edge AI, offering interactive 
+            experiences and personalized companionship.
             Please use Use the information from the DOCUMENTS section to deliver precise responses, presenting the knowledge 
-            as if it were innate. If uncertain, plainly admit to not knowing and explain that you are a virtual agent. 
+            as if it was innate. If uncertain, plainly admit to not knowing and explain that you are a virtual agent. 
             Ask always for the user's name and use it in the conversation.
                     
             DOCUMENTS:
